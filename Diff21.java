71dcd6f6762bd2cb99229324f72c303873eadf00
Jules95Game <romp.julian1@gmail.com>
2024-02-06 14:56:03 +0100

    Diff21

---
+++ Diff21.java
@@ -0,0 +1,17 @@
+public class Diff21 {
+  public static void main(String[] args) {
+    System.out.println(diff21(19)); //  2
+    System.out.println(diff21(10)); // 11
+    System.out.println(diff21(21)); //  0
+    System.out.println(diff21(22)); //  2
+    System.out.println(diff21(34)); // 26
+  }
+
+  private static int diff21(int n) {
+    if (n > 21) {
+      return Math.abs(n - 21) * 2;
+    } else {
+      return Math.abs(n - 21);
+    }
+  }
+}
