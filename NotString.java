bfdc3e214721d7224e7947f4a27f9f9407571603
Jules95Game <romp.julian1@gmail.com>
2024-02-12 12:49:54 +0100

    NotString

---
+++ NotString.java
@@ -0,0 +1,14 @@
+public class NotString {
+  public static void main(String[] args) {
+    System.out.println(notString("candy")); // "not candy"
+    System.out.println(notString("x")); // "not x"
+    System.out.println(notString("not bad")); // "not bad"
+    System.out.println(notString("XP")); // "not XP"
+  }
+
+  private static String notString(String input) {
+    if (input.length() >= 3 && input.substring(3).equals("not")) return input;
+
+    return "not " + input;
+  }
+}
