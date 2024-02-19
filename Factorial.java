94e0ed357510d7833aca75fdb90b620c64c07796
Jules95Game <romp.julian1@gmail.com>
2024-02-19 15:33:51 +0100

    WIP Factorial fix

--- Factorial.java
+++ Factorial.java
@@ -5,11 +5,10 @@ public class Factorial {
     System.out.print("Please enter an integer greater then zero: ");
     int userChoice = StringToInteger.parseNatural();
 
-    int last = 0;
     int result = 1;
     for (int i = userChoice; i > 0; i--) {
-      last = i;
       result *= i;
+      // ToDo: use BigInteger
       if (result < 0) {
         System.out.println("The result is larger then a 32-bit integer can hold.");
         return;
