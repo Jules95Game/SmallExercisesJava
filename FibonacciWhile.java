79918575ae869549d671c26297903e838b1e078f
Jules95Game <romp.julian1@gmail.com>
2024-02-26 12:07:39 +0100

    Removed useless code

--- FibonacciWhile.java
+++ FibonacciWhile.java
@@ -5,24 +5,17 @@ public class FibonacciWhile {
     System.out.print("Up to which number do you want to generate Fibonacci numbers? ");
     int userChoice = StringToInteger.parseNatural();
 
-    if (userChoice == 1) {
-      System.out.println("1, 1");
-      return;
-    } else if (userChoice == 2) {
-      System.out.println("1, 1, 2");
-      return;
-    }
-
     int secondLast = 1;
     int last = 1;
     int fib = secondLast + last;
     System.out.print(secondLast + ", " + last);
-    do {
+
+    while (fib <= userChoice) {
       System.out.print(", " + fib);
       secondLast = last;
       last = fib;
       fib = secondLast + last;
       if (fib < 1) break;
-    } while (fib <= userChoice);
+    }
   }
 }
